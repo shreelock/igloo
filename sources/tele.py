0413c2e86d961ae64646a2e10087fd1ac5225851
Shreenath <sh101094@outlook.com>
2024-04-14 17:52:49 -0700

    Added time in status

--- sources/tele.py
+++ sources/tele.py
@@ -20,7 +20,8 @@ UPPER_LIMIT = 150
 LOWER_LIMIT = 70
 
 MINS = 60
-POLL_INTERVAL = 5*MINS
+POLL_INTERVAL = 5 * MINS
+
 
 # @bot.message_handler(func=lambda msg: True)
 # def echo_all(message):
@@ -38,33 +39,70 @@ class LibreToken:
             self.token = login(LIBRE_EMAIL, LIBRE_PWD)
 
 
+class CurrStatus:
+    def __init__(self):
+        self.time_in_status_mins = 0
+        self.status_entry_time = 0
+        self.last_value = None
+
+    def update(self, val, input_time):
+        if not self.status_entry_time:
+            self.status_entry_time = input_time
+        self.time_in_status_mins = (input_time - self.status_entry_time).seconds // 60
+        self.last_value = val
+
+    def reset(self):
+        self.__init__()
+
 def fetch_latest_reading(libre_token: LibreToken):
     cgm_data = get_cgm_data(token=libre_token.token, patient_id=libre_token.patient_id)
     libre_token.expires = cgm_data['ticket']['expires']
     glucose_value = cgm_data['data']['connection']['glucoseItem']['ValueInMgPerDl']
     return glucose_value
 
+
 def send_message(message_text):
     bot.send_message(chat_id=CHAT_ID, text=message_text)
+
+def value_out_of_range(value):
+    if value <= LOWER_LIMIT or value >= UPPER_LIMIT:
+        return True
+    return False
+
+def prepare_message(value, status_obj: CurrStatus):
+    time_in_status = status_obj.time_in_status_mins
+    text_str = f"{value}, Time in Status : {time_in_status} mins"
+    return text_str
+
+
+def get_poll_interval(value):
+    if value <= LOWER_LIMIT:
+        return 1 * MINS
+    elif value >= UPPER_LIMIT:
+        return 2 * MINS
+    else:
+        return 5 * MINS
 
 
 if __name__ == '__main__':
     _token = LibreToken()
+    _status = CurrStatus()
     while True:
+        _curr_time = datetime.datetime.now()
         _token.refresh()
         try:
-            latest_val = fetch_latest_reading(_token)
-            if latest_val >= UPPER_LIMIT:
-                send_message(message_text=latest_val)
-                POLL_INTERVAL = 2 * MINS
-            elif latest_val <= LOWER_LIMIT:
-                send_message(message_text=latest_val)
-                POLL_INTERVAL = 1 * MINS
+            _latest_val = fetch_latest_reading(_token)
+            if value_out_of_range(_latest_val):
+                _status.update(_latest_val, _curr_time)
+                send_message(message_text=prepare_message(_latest_val, _status))
             else:
-                POLL_INTERVAL = 5 * MINS
+                _status.reset()
 
-            print(f"{datetime.datetime.now()}, Current Reading is {latest_val}")
+            POLL_INTERVAL = get_poll_interval(_latest_val)
+            print(f"{_curr_time}, Current Reading is {_latest_val}")
             time.sleep(POLL_INTERVAL)
+
+
         except Exception as exd:
             send_message(f"Processing failed. Exception = {exd}")
             break
